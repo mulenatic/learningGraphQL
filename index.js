5ff1f997375f436f0797142a93292c687635be66
user <user>
2020-03-25 09:11:26 +0000

    Added example users and photos in memory.

--- index.js
+++ index.js
@@ -41,7 +41,34 @@ type Mutation {
 }
 `
 var _id = 0;
-var photos = [];
+var users = [
+    { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+    { "githubLogin": "gPlake", "name": "Glen Plake" },
+    { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+
+var photos = [
+    {
+	"id": "1",
+	"name": "Dropping the Heart Chute",
+	"description": "The heart chute is one of my favorite chutes",
+	"category": "ACTION",
+	"githubUser": "gPlake"
+    },
+    {
+	"id": "2",
+	"name": "Enjoying the sunshine",
+	"category": "SELFIE",
+	"githubUser": "sSchmidt"
+    },
+    {
+	id: "3",
+	"name": "Gunbarrel 25",
+	"description": "25 laps on gunbarrel today",
+	"category": "LANDSCAPE",
+	"githubUser": "sSchmidt"
+    }
+]
 
 const resolvers = {
     Query: {
