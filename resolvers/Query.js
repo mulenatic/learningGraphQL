fdb0208ad822aff6351fc8417a2560d11997e208
user <user>
2020-03-26 09:16:57 +0000

    Added Queries and resolvers for users.

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,6 +1,22 @@
 const { photos } = require('./dataStore');
 
 module.exports =  {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos
+    totalPhotos: (parent, args, { db }) =>
+	db.collection('photos')
+	  .estimatedDocumentCount(),
+
+    allPhotos: (parent, args, { db }) =>
+	db.collection('photos')
+	  .find()
+	  .toArray(),
+    
+    totalUsers: (parent, args, { db }) =>
+	db.collection('users')
+	  .estimatedDocumentCount(),
+
+    allUsers: (parent, args, { db }) =>
+	db.collection('users')
+	  .find()
+	  .toArray()
+    
 };
